2ced2c547f9f051c7f4b369ad22ca3fd77b3ff42
n-fukukawa <fukulab11@icloud.com>
2025-02-13 05:12:18 +0900

    fix: index out of range

--- MinimalThings/View/MyItem/MyItemListItem.swift
+++ MinimalThings/View/MyItem/MyItemListItem.swift
@@ -11,31 +11,33 @@ struct MyItemListItem: View {
   let item: Item
   
   var body: some View {
-    VStack(spacing: 0) {
-      HStack(spacing: 0) {
-        if let uiImage = UIImage(data: item.images[0]) {
-          Image(uiImage: uiImage)
-            .resizable()
-            .frame(width: 80, height: 80)
-            .aspectRatio(contentMode: .fill)
-        } else {
+    if item.images.count > 0 {
+      VStack(spacing: 0) {
+        HStack(spacing: 0) {
+          if let uiImage = UIImage(data: item.images[0]) {
+            Image(uiImage: uiImage)
+              .resizable()
+              .frame(width: 80, height: 80)
+              .aspectRatio(contentMode: .fill)
+          } else {
+            Text(item.name)
+              .font(.caption)
+              .frame(width: 80, height: 80)
+            Rectangle()
+              .fill(Color(UIColor.systemGray5).opacity(0.5)).frame(width: 1)
+          }
+          
           Text(item.name)
-            .font(.caption)
-            .frame(width: 80, height: 80)
-          Rectangle()
-            .fill(Color(UIColor.systemGray5).opacity(0.5)).frame(width: 1)
+            .font(.subheadline)
+            .lineLimit(2)
+            .padding(.leading, 8)
+          
+          Spacer()
         }
-        
-        Text(item.name)
-          .font(.subheadline)
-          .lineLimit(2)
-          .padding(.leading, 8)
-        
-        Spacer()
+        Rectangle()
+          .fill(Color(UIColor.systemGray5).opacity(0.5))
+          .frame(height: 1)
       }
-      Rectangle()
-        .fill(Color(UIColor.systemGray5).opacity(0.5))
-        .frame(height: 1)
     }
   }
 }
