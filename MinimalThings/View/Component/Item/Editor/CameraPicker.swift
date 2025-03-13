d0b2b470809604f7a50ed2a385ef659902aba562
n-fukukawa <fukulab11@icloud.com>
2025-03-14 08:14:03 +0900

    fix: camera

--- MinimalThings/View/Component/Item/Editor/CameraPicker.swift
+++ MinimalThings/View/Component/Item/Editor/CameraPicker.swift
@@ -1,41 +1,92 @@
+////
+////  CameraPicker.swift
+////  MinimalThings
+////
+////  Created by Naruki Fukukawa on 2025/02/27.
+////
 //
-//  CameraPicker.swift
-//  MinimalThings
+//import SwiftUI
+//import AVFoundation
 //
-//  Created by Naruki Fukukawa on 2025/02/27.
+//struct CameraPicker: View {
+//  @Environment(\.dismiss) var dismiss
+//  let onAfterPick: () -> Void
+//  @Binding var image: UIImage?
+//  @State private var showCamera = false
+//  @State private var available = false
+//  @State private var showAlert = false
+//  
+//  var body: some View {
+//    NavigationView {
+//      VStack {
+//        if !available {
+//          VStack (spacing: 10) {
+//            Text("Camera access is not permitted.")
+//            Button("Open settings") { openSettings() }
+//          }
+//        }
+//        
+//        if available {
+//          Rectangle()
+//            .fill(.clear)
+//            .frame(height: 1)
 //
-
-import SwiftUI
-
-struct CameraPicker: View {
-  @Environment(\.dismiss) var dismiss
-  let onAfterPick: () -> Void
-  @Binding var image: UIImage?
-  @State private var showCamera = true
-  
-  var body: some View {
-    NavigationView {
-      Rectangle()
-        .fill(.clear)
-        .frame(height: 1)
-        .fullScreenCover(isPresented: self.$showCamera) {
-          ImagePickerView(image: $image, isPresented: $showCamera)
-        }
-    }
-    .onChange(of: image) { _, newValue in
-      if newValue != nil {
-        onAfterPick()
-      }
-    }
-    .onChange(of: showCamera) { _, newValue in
-      if !newValue {
-        dismiss()
-      }
-    }
-  }
-}
-
-#Preview {
-  @Previewable @State var image: UIImage?
-  CameraPicker(onAfterPick: {}, image: $image)
-}
+//        }
+//      }
+//    }
+//    .alert("Camera access is not permitted.", isPresented: $showAlert) {
+//      Button("Cancel") { showAlert = false }
+//      Button("Open settings") { openSettings() }
+//    } message: {
+//      Text("Please allow camera access in iOS settings.")
+//    }
+//    .onAppear {
+//      checkCamera()
+//    }
+//    .onChange(of: image) { _, newValue in
+//      if newValue != nil {
+//        onAfterPick()
+//      }
+//    }
+//    .onChange(of: showCamera) { _, newValue in
+//      if !newValue {
+//        dismiss()
+//      }
+//    }
+//  }
+//  
+//  private func checkCamera() {
+//    let status = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+//    
+//    if status == .notDetermined {
+//      AVCaptureDevice.requestAccess(for: .video) { (granted: Bool) in
+//          DispatchQueue.main.async {
+//            if granted {
+//              showCamera = true
+//              available = true
+//            }
+//          }
+//      }
+//    } else if status == AVAuthorizationStatus.authorized {
+//      showCamera = true
+//      available = true
+//    } else {
+//      AVCaptureDevice.requestAccess(for: .video, completionHandler: { granted in
+//        if (granted == false) {
+//          showAlert = true
+//          available = false
+//          print("not available")
+//        }
+//      })
+//    }
+//  }
+//  
+//
+//}
+//
+//
+//
+//#Preview {
+//  @Previewable @State var image: UIImage?
+//  CameraPicker(onAfterPick: {}, image: $image)
+//}
--- MinimalThings/View/Component/Item/Editor/PhotoPicker.swift
+++ MinimalThings/View/Component/Item/Editor/PhotoPicker.swift
@@ -14,54 +14,109 @@ struct PhotoPicker: View {
   // Album
   @State private var photosPickerItem: PhotosPickerItem? = nil
   @State private var photoDataState: Data? = nil
+  
   // Camera
+  @State private var isPresented: Bool = false
+  @State private var showCamera: Bool = false
+  @State private var showAlert: Bool = false
   @State private var cameraImage: UIImage? = nil
   
   var body: some View {
-    TabView {
-      Tab("Album", systemImage: "photo") {
-        PhotosPicker(
-          selection: $photosPickerItem,
-          matching: .images,
-          photoLibrary: .shared()
-        ) {}
-          .photosPickerStyle(.inline)
-          .photosPickerDisabledCapabilities([.selectionActions])
-          .photosPickerAccessoryVisibility(.hidden, edges: .all)
-          .task(id: photosPickerItem) {
-            if let data = try? await photosPickerItem?.loadTransferable(type: Data.self) {
-              photoDataState = UIImage(data: data)?
-                .preparingThumbnail(of: CGSize(width: 1000, height: 1000))?
-                .jpegData(compressionQuality: 0.5)
-            }
-          }
-      }
-      
-      Tab("Camera", systemImage: "camera") {
-        CameraPicker(
-          onAfterPick: {
-            photoData = cameraImage?
+    VStack {
+      PhotosPicker(
+        selection: $photosPickerItem,
+        matching: .images,
+        photoLibrary: .shared()
+      ) {}
+        .photosPickerStyle(.inline)
+        .photosPickerDisabledCapabilities([.selectionActions])
+        .photosPickerAccessoryVisibility(.hidden, edges: .all)
+        .task(id: photosPickerItem) {
+          if let data = try? await photosPickerItem?.loadTransferable(type: Data.self) {
+            photoDataState = UIImage(data: data)?
               .preparingThumbnail(of: CGSize(width: 1000, height: 1000))?
               .jpegData(compressionQuality: 0.5)
-          },
-          image: $cameraImage)
+          }
+        }
+        .toolbar {
+          ToolbarItem(placement: .topBarLeading) {
+            Button("Cancel") {
+              photosPickerItem = nil
+              dismiss()
+            }
+          }
+          ToolbarItem(placement: .topBarTrailing) {
+            Button("Done") {
+              photoData = photoDataState
+              photosPickerItem = nil
+              dismiss()
+            }
+          }
+        }
+      
+      HStack {
+        Spacer()
+        Button { showCamera = false } label: {
+          VStack(spacing: 4) {
+            Image(systemName: "photo").font(.title3)
+            Text("Album").font(.caption)
+          }
+          .tint(.accentColor)
+        }
+        Spacer()
+        Button {
+          let status = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+          
+          if status == .notDetermined {
+            AVCaptureDevice.requestAccess(for: .video) { (granted: Bool) in
+                if granted {
+                  DispatchQueue.main.async {
+                    showCamera = true
+                  }
+                }
+            }
+          } else if status == AVAuthorizationStatus.authorized {
+            showCamera = true
+          } else {
+            showAlert = true
+          }
+        } label: {
+          VStack(spacing: 4) {
+            Image(systemName: "camera").font(.title3)
+            Text("Camera").font(.caption)
+          }
+          .tint(.foregroundTertiary)
+        }
+        Spacer()
+      }
+      .padding(.vertical, 10)
+    }
+    .fullScreenCover(isPresented: $showCamera) {
+      ImagePickerView(image: $cameraImage, isPresented: $showCamera)
+    }
+    .onChange(of: isPresented) { _, newValue in
+      showCamera = newValue
+    }
+    .alert("Camera access is not permitted.", isPresented: $showAlert) {
+      Button("Cancel") { showAlert = false }
+      Button("Open settings") { openSettings() }
+    } message: {
+      Text("Please allow camera access in iOS settings.")
+    }
+    .onChange(of: cameraImage) { _, newValue in
+      if newValue != nil {
+        photoData = cameraImage?
+          .preparingThumbnail(of: CGSize(width: 1000, height: 1000))?
+          .jpegData(compressionQuality: 0.5)
       }
     }
-    .toolbar {
-      ToolbarItem(placement: .topBarLeading) {
-        Button("Cancel") {
-          photosPickerItem = nil
-          dismiss()
-        }
-      }
-      ToolbarItem(placement: .topBarTrailing) {
-        Button("Done") {
-          photoData = photoDataState
-          photosPickerItem = nil
-          dismiss()
-        }
-      }
+  }
+  
+  private func openSettings() {
+    guard let settingsURL = URL(string: UIApplication.openSettingsURLString ) else {
+      return
     }
+    UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
   }
 }
 
